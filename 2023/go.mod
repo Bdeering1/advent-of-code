1ea2cf3874e5649c394e5dbff2f1070605790b5a
Bryn Deering <bdeering@uoguelph.ca>
2023-12-11 18:45:11 -0500

    Day 5 part 2 solution draft

--- 2023/go.mod
+++ 2023/go.mod
@@ -1,3 +1,8 @@
 module github.com/bdeering1/advent-of-code/2023
 
 go 1.21.4
+
+require (
+	github.com/Jeffail/tunny v0.1.4 // indirect
+	github.com/alitto/pond v1.8.3 // indirect
+)
--- 2023/solutions/day5.go
+++ 2023/solutions/day5.go
@@ -1,10 +1,12 @@
 package solutions
 
 import (
-    "math"
-    "regexp"
-    "strconv"
-    "strings"
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 func (Solutions) Day5_1(input []string) int {
@@ -59,5 +61,91 @@ func (Solutions) Day5_1(input []string) int {
 }
 
 func (Solutions) Day5_2(input []string) int {
-    return -1
+    num_pat := regexp.MustCompile("[0-9]+")
+
+    min_locs := []int64{}
+    var wg sync.WaitGroup
+    receiver := func(channel <-chan int64) {
+	for {
+	    val := <- channel
+	    fmt.Printf("received %d\n", val)
+	    if val == -1 {
+		break
+	    }
+	    min_locs = append(min_locs, val)
+	}
+    }
+    channel := make(chan int64)
+    go receiver(channel)
+
+    var seed_start int64 = -1
+    for _, seed_str := range num_pat.FindAllString(input[0], -1) {
+	num, _ := strconv.ParseInt(seed_str, 10, 64)
+
+	if seed_start == -1 {
+	    seed_start = num
+	} else {
+	    s_start := seed_start
+	    seed_range := num
+	    channel := channel
+
+	    wg.Add(1)
+	    go func() {
+		var min_loc int64 = math.MaxInt64
+		fmt.Printf("Starting range %d + %d\n", s_start, seed_range)
+		defer wg.Done()
+
+		// loop through seed range
+		var i int64
+		for i = 0; i < seed_range; i++ {
+		    map_input := s_start + i
+		    modified := false
+
+		    // run input through each map
+		    for _, line := range input[2:] {
+			if strings.Contains(line, ":") {
+			    // start map
+			    modified = false
+			    continue
+			}
+			if modified || len(line) == 0 {
+			    // end map
+			    continue
+			}
+
+			nums := num_pat.FindAllString(line, 3)
+			dest_start, _ := strconv.ParseInt(nums[0], 10, 64)
+			input_start, _ := strconv.ParseInt(nums[1], 10, 64)
+			range_len, _ := strconv.ParseInt(nums[2], 10, 64)
+
+			// transform input if it falls within input range
+			if map_input >= input_start && map_input < input_start + range_len {
+			    map_input = dest_start + (map_input - input_start)
+			}
+		    }
+		    // find lowest location #
+		    if map_input < min_loc {
+			min_loc = map_input
+		    }
+		}
+
+		channel <- min_loc
+	    }()
+
+	    seed_start = -1
+	}
+    }
+
+    wg.Wait()
+    channel <- -1
+    close(channel)
+
+    var min_loc int64 =  math.MaxInt64
+    for _, loc := range min_locs {
+	if loc < min_loc {
+	    min_loc  = loc
+	}
+    }
+
+    return int(min_loc)
 }
